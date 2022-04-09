0c7a16c4a051320acbe7add9bd8d6741f660008d
gadaharsh <gada.hp@somaiya.edu>
2022-04-10 02:00:23 +0530

    radio buttons on all steps

--- client/src/Components/Causes/causes.js
+++ client/src/Components/Causes/causes.js
@@ -5,17 +5,20 @@ import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
 import { Link } from 'react-router-dom'
 import { Button } from '@mui/material';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 import "./causes.css";
-
-
 
 const Causes = () => {
   return (
     <div className="container sym">
      <Typography>These diseases and conditions match at least one of the factors you selected. Those with the most matches are listed first.</Typography>
     <div className="container cause">
-    <div>
+    {/* <div>
       
     <Button component={Link} to="/" type="button">
     Gastritis
@@ -25,7 +28,27 @@ const Causes = () => {
 <Button component={Link} to="/" type="button">
 Peptic ulcer
 </Button>
-</div>
+</div> */}
+<FormControl>
+      {/* <FormLabel id="demo-radio-buttons-group-label">Accompanied By</FormLabel> */}
+      <RadioGroup
+        aria-labelledby="demo-radio-buttons-group-label"
+        defaultValue="Symptom Name"
+        name="radio-buttons-group"
+      >
+        <div className="rad">
+        <Row>
+         <Col sm>
+        <FormControlLabel  className="radio" value="1" control={<Radio />} label="Gastritis" />
+        </Col> 
+        <Col sm>
+        <FormControlLabel  className="radio" value="2" control={<Radio />} label="Peptic ulcer" />
+        </Col>
+        </Row>  
+       
+        </div>
+      </RadioGroup>
+    </FormControl>
 
     </div>
 
