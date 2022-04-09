b589629e917e79c4393ee0cf47e8df7876539eee
meetgandhii <meetgandhism@gmail.com>
2022-04-10 01:05:34 +0530

    all steps done- harsh

--- client/src/Components/Causes/causes.js
+++ client/src/Components/Causes/causes.js
@@ -4,7 +4,7 @@ import { Grid, Typography } from "@mui/material";
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
 import { Link } from 'react-router-dom'
-
+import { Button } from '@mui/material';
 
 import "./causes.css";
 
@@ -14,9 +14,18 @@ const Causes = () => {
   return (
     <div className="container sym">
      <Typography>These diseases and conditions match at least one of the factors you selected. Those with the most matches are listed first.</Typography>
-    <div className="container">
-    
-
+    <div className="container cause">
+    <div>
+      
+    <Button component={Link} to="/" type="button">
+    Gastritis
+</Button>
+</div>
+<div>
+<Button component={Link} to="/" type="button">
+Peptic ulcer
+</Button>
+</div>
 
     </div>
 
