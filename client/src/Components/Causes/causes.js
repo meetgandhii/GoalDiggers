1c389a02bbe009f9cafe368ff940bce6f7c1dfc5
meetgandhii <meetgandhism@gmail.com>
2022-04-10 00:43:11 +0530

    step3

---
+++ client/src/Components/Causes/causes.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Card, Row ,Col} from "react-bootstrap";
+import { Grid, Typography } from "@mui/material";
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import { Link } from 'react-router-dom'
+
+
+import "./causes.css";
+
+
+
+const Causes = () => {
+  return (
+    <div className="container sym">
+     <Typography>These diseases and conditions match at least one of the factors you selected. Those with the most matches are listed first.</Typography>
+    <div className="container">
+    
+
+
+    </div>
+
+</div>
+  );
+};
+
+export default Causes;
