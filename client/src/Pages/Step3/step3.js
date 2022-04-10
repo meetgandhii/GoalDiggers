35dec95fb2db683eed96310f5243a398b1048e9a
gadaharsh <gada.hp@somaiya.edu>
2022-04-10 06:10:48 +0530

    handle click

--- client/src/Pages/Step3/step3.js
+++ client/src/Pages/Step3/step3.js
@@ -1,21 +1,28 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 import "./step3.css";
 
-import Symptom from '../../Components/SypmtomCard/symptom';
-import Factors from '../../Components/Factors/factors';
-import Causes from '../../Components/Causes/causes';
-import SymptomChild from '../../Components/SypmtomChild/symptom';
+import Symptom from "../../Components/SypmtomCard/symptom";
+import Factors from "../../Components/Factors/factors";
+import Causes from "../../Components/Causes/causes";
+import SymptomChild from "../../Components/SypmtomChild/symptom";
 
-const steps = ['Choose a Symptom', 'Select Related Factors', 'View Possible Causes'];
+const { symptom } = require('../../Components/SypmtomChild/symptom')
+
+const steps = [
+  "Choose a Symptom",
+  "Select Related Factors",
+  "View Possible Causes",
+];
 
 export default function Step2_child() {
+
   const [activeStep, setActiveStep] = React.useState(0);
   const [skipped, setSkipped] = React.useState(new Set());
 
@@ -23,7 +30,6 @@ export default function Step2_child() {
     return step === 1;
   };
 
-  
   const isStepSkipped = (step) => {
     return skipped.has(step);
   };
@@ -63,81 +69,80 @@ export default function Step2_child() {
   };
 
   return (
-     <>
-     <div className='container step2'>
-    <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const stepProps = {};
-          const labelProps = {};
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
-          }
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
-      {activeStep === steps.length ? (
-        <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
-          </Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Box sx={{ flex: '1 1 auto' }} />
-            <Button onClick={handleReset}>Reset</Button>
-          </Box>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-            {/* write step details here */}
-            <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
-          {(() => {
-      
+    <>
+      <div className="container step2">
+        <Box sx={{ width: "100%" }}>
+          <Stepper activeStep={activeStep}>
+            {steps.map((label, index) => {
+              const stepProps = {};
+              const labelProps = {};
+              if (isStepSkipped(index)) {
+                stepProps.completed = false;
+              }
+              return (
+                <Step key={label} {...stepProps}>
+                  <StepLabel {...labelProps}>{label}</StepLabel>
+                </Step>
+              );
+            })}
+          </Stepper>
+          {activeStep === steps.length ? (
+            <React.Fragment>
+              <Typography sx={{ mt: 2, mb: 1 }}>
+                All steps completed - you&apos;re finished
+              </Typography>
+              <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                <Box sx={{ flex: "1 1 auto" }} />
+                <Button onClick={handleReset}>Reset</Button>
+              </Box>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              {/* write step details here */}
+              <Typography sx={{ mt: 2, mb: 1 }}>
+                Step {activeStep + 1}
+              </Typography>
+              {(() => {
+                if (activeStep == 0) {
+                  return (
+                    <>
+                      <SymptomChild />
+                      
+                    </>
+                  );
+                } else if (activeStep == 1) {
+                  return (
+                    <>
+                      <Factors />
+                    </>
+                  );
+                } else if (activeStep == 2) {
+                  return (
+                    <>
+                      <Causes />
+                    </>
+                  );
+                }
+              })()}
 
-      if(activeStep == 0){
-        return(<>
-<SymptomChild/>
-
-</>
-          )
-      }else if(activeStep == 1){
-
-        return(<>
-          
-          <Factors/>
-          </>)
-
-      }else if(activeStep == 2){
-
-        return(<>
-          <Causes/>
-          </>)
-
-      }
-    
-    }    )()}
-          
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Back
-            </Button>
-            <Box sx={{ flex: '1 1 auto' }} />
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            </Button>
-          </Box>
-        </React.Fragment>
-      )}
-    </Box>
-    </div>
+              <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                <Button
+                  color="inherit"
+                  disabled={activeStep === 0}
+                  onClick={handleBack}
+                  sx={{ mr: 1 }}
+                >
+                  Back
+                </Button>
+                <Box sx={{ flex: "1 1 auto" }} />
+                <Button onClick={handleNext}>
+                  {activeStep === steps.length - 1 ? "Finish" : "Next"}
+                </Button>
+              </Box>
+            </React.Fragment>
+          )}
+        </Box>
+      </div>
     </>
   );
 }
